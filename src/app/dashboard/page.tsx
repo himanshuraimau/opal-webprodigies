a40d407754270a3c1daab3352114cb16f0af5809
Himanshu Rai <himanshuraimau9@gmail.com>
2024-11-01 19:41:18 +0530

    add authentication logic to dashboard and callback pages; implement loading spinner component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { onAuthenticateUser } from '@/actions/user'
+import { redirect } from 'next/navigation'
 
 
-const DashboardPage = () => {
 
+const DashboardPage = async() => {
+    const auth  = await onAuthenticateUser()
+    if(auth.status === 200 || auth.status === 201){
+        return redirect(`/dashboard/${auth.user?.firstname}${auth.user?.lastname}`)
+    }
+    if(auth.status === 400 || auth.status === 500 || auth.status === 404){
+        return redirect('/auth/sign-in')
+    }
 
   return (
-    <div>DashboardPage</div>
+    redirect('/auth/sign-in')
   )
 }
 
