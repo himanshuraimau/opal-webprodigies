b8d2d9495c7f980f59da32ee759689da6135950a
Himanshu Rai <himanshuraimau9@gmail.com>
2024-11-01 19:29:44 +0530

    add user, comment, subscription, media, workspace, folder, video, member, notification, and invite models; update user authentication logic

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,9 +4,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined 
 };
 
-export const prisma = 
+export const client = 
   globalForPrisma.prisma ?? 
   new PrismaClient({
+     log: ['query', 'info', 'warn', 'error']
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
